10cb538adf3fe3a8fc1bab7ffc1713c6e378747f
Ryan <ryamcmorris@gmail.com>
2024-06-02 16:00:16 -0500

    goals

--- app/models.py
+++ app/models.py
@@ -1,23 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-
 db = SQLAlchemy()
 
 class Parent(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable = False)
-    role = db.Column(db.String(10), nullable=False, default="parent") # "parent or child"
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String(10), nullable=False, default="parent")  # "parent or child"
     children = db.relationship('Child', backref='parent', lazy=True)
-
-    # def __init__(self, id, first_name, email, password, role):
-    #     self.id = id
-    #     self.first_name = first_name
-    #     self.email = email
-    #     self.password = generate_password_hash(password)
-    #     self.role = role
 
     def save(self):
         db.session.add(self)
@@ -27,18 +19,11 @@ class Child(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'), nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable = False)
-    role = db.Column(db.String(10), nullable=False, default="child") # "parent or child"
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String(10), nullable=False, default="child")  # "parent or child"
     chores = db.relationship('Chores', backref='child', lazy=True)
     wallet = db.relationship('Wallet', uselist=False, back_populates='child')
     goals = db.relationship('Goal', back_populates='child', lazy=True)
-
-    # def __init__(self, id, parent_id, username, password, role):
-    #     self.id = id
-    #     self.parent_id = parent_id
-    #     self.username = username
-    #     self.password = generate_password_hash(password)
-    #     self.role = role
 
     def save(self):
         db.session.add(self)
@@ -51,16 +36,9 @@ class Chores(db.Model):
     is_completed = db.Column(db.Boolean, default=False)
     frequency = db.Column(db.String, nullable=False)
 
-    # def __init__(self, child_id, name, is_completed, frequency):
-    #     self.child_id = child_id
-    #     self.name = name
-    #     self.is_completed = is_completed
-    #     self.frequency = frequency
-
     def save(self):
         db.session.add(self)
         db.session.commit()
-
 
 class Wallet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -68,7 +46,7 @@ class Wallet(db.Model):
     amount = db.Column(db.Float, nullable=False, default=0.0)
     goal_account = db.Column(db.Float, nullable=False, default=0.0)
     child = db.relationship('Child', back_populates='wallet')
-    goals =db.relationship('Goal', back_populates='wallet', lazy=True)
+    goals = db.relationship('Goal', back_populates='wallet', lazy=True)
 
     def save(self):
         db.session.add(self)
@@ -85,15 +63,12 @@ class Wallet(db.Model):
         return self.amount
     
     def transfer_amount(self, amount):
-       if self.amount >= amount:
-           self.amount -= amount
-           self.goal_account += amount
-           self.save()
-           return True
-       return False
-    
-
-
+        if self.amount >= amount:
+            self.amount -= amount
+            self.goal_account += amount
+            self.save()
+            return True
+        return False
 
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -104,7 +79,7 @@ class Goal(db.Model):
     link = db.Column(db.String, nullable=True)
     description = db.Column(db.Text, nullable=True)
     wallet_id = db.Column(db.Integer, db.ForeignKey('wallet.id'), nullable=False)
-    wallet = db.relationship('Wallet', backref='goals', lazy=True)
+    wallet = db.relationship('Wallet', back_populates='goals')
     child = db.relationship('Child', back_populates='goals')
 
     def save(self):
