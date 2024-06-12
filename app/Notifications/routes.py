353845dc5a9a2ee0895950d8f60c079659504452
Ryan <ryamcmorris@gmail.com>
2024-06-12 12:49:22 -0500

    added notifications

---
+++ app/Notifications/routes.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify
+from app.models import Notifications, Child, Goal, Chores
+
+notifications = Blueprint('notifications', __name__)
+
+@notifications.route('/notifications/<int:parent_id>', methods=['GET'])
+def get_notifications(parent_id):
+    
+    children = Child.query.filter_by(parent_id=parent_id).all()
+
+    
+    notifications_list = []
+
+  
+    for child in children:
+       
+        completed_goals = Goal.query.filter_by(child_id=child.id, complete=True).all()
+        completed_chores = Chores.query.filter_by(child_id=child.id, status='complete').all()
+
+       
+        for goal in completed_goals:
+            notification = {
+                'child_name': child.username,
+                'type': 'goal',
+                'name': goal.name
+            }
+            notifications_list.append(notification)
+        for chore in completed_chores:
+            notification = {
+                'child_name': child.username,
+                'type': 'chore',
+                'name': chore.name
+            }
+            notifications_list.append(notification)
+
+   
+    return jsonify(notifications_list)
+
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta, timezone
 import jwt
 import pytz
 
+
 db = SQLAlchemy()
 
 class Parent(db.Model, UserMixin):
@@ -85,10 +86,14 @@ class Chores(db.Model):
     due_date = db.Column(db.Date, nullable=True)
     amount = db.Column(db.Integer, nullable=False)
 
+    
     def save(self):
         db.session.add(self)
         db.session.commit()
-        
+
+    def get_child_username(self):
+        return self.child.username
+
     def to_dict(self):
         return {
             "id": self.id,
@@ -99,7 +104,7 @@ class Chores(db.Model):
             "frequency": self.frequency,
             "due_date": self.due_date.isoformat() if self.due_date else None,
             "amount": self.amount
-        }
+        } 
 
 class Wallet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -140,9 +145,44 @@ class Goal(db.Model):
     img = db.Column(db.String, nullable=True)
     link = db.Column(db.String, nullable=True)
     description = db.Column(db.Text, nullable=True)
+    complete = db.Column(db.Boolean, nullable=True, default=False)
     wallet_id = db.Column(db.Integer, db.ForeignKey('wallet.id'), nullable=False)
     wallet = db.relationship('Wallet', back_populates='goals')
     child = db.relationship('Child', back_populates='goals')
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def get_child_username(self):
+        return self.child.username
+  
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "child_id": self.child_id,
+            "name": self.name,
+            "amount": self.amount,
+            "paid": self.paid,
+            "img": self.img,
+            "link": self.link,
+            "description": self.description,
+            "complete": self.complete,
+            "wallet_id": self.wallet_id
+        }  
+ 
+
+class Notifications(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    child_id = db.Column(db.Integer, db.ForeignKey("child.id"), nullable=False)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.id"), nullable=True)
+    child_username = db.Column(db.String, db.ForeignKey("child.username"), nullable=False)
+    chores_id = db.Column(db.Integer, db.ForeignKey("chores.id"), nullable=True)
+    goal_name = db.Column(db.String, nullable=True)
+    chores_name = db.Column(db.String, nullable=True)
+    chores_status = db.Column(db.String, nullable=True)
+    goal_complete = db.Column(db.Boolean, nullable=True)
 
     def save(self):
         db.session.add(self)
